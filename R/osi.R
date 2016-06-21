7e803a981f72283359ef8b134c6497d6d4a5aab6
Oliver Keyes <Ironholds@users.noreply.github.com>
2016-06-21 09:05:23 -0400

    copyedit

--- R/osi.R
+++ R/osi.R
@@ -1,6 +1,6 @@
 #' @title Connector to the Open Source Initiative API
 #' @name osi
-#' @description This package provides A connector to the API maintained by the
+#' @description This package provides a connector to the API maintained by the
 #' \href{Open Source Initiative}{https://api.opensource.org/licenses/}, which
 #' provides machine-readable metadata about a variety of open source software licenses.
 #' @docType package
